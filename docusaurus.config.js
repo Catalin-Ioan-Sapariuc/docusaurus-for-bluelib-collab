b344d33b31f3c4dfdf05e533d545bfbb4b2b590a
Catalin-Ioan-Sapariuc <ioan.sapariuc@cultofcoders.com>
2022-11-10 14:42:11 +0200

    Added github links to docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,8 +16,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'github.com/Catalin-Ioan-Sapariuc', // Usually your GitHub org/user name.
+  projectName: 'docusaurus-for-bluelib-collab', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -37,7 +37,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Catalin-Ioan-Sapariuc/docusaurus-for-bluelib-collab.git/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
